82f5cb10587dc562e83bab3f9f6e8190bbd74d7e
Dhanush <165892573+DOITGFD@users.noreply.github.com>
2024-09-30 09:35:05 +0530

    to find smallest number

---
+++ smallest num.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int arr[ ]={8,2,5,1,9,6};
+{
+    int i=0
+    int min=(int arr[],int n);
+    int min= arr[];
+    for i in range (i=0,i<n,i++);
+    {
+        if (arr[i] < min);
+        min=arr[i]
+    }
+    return min;
+}
